f343b3c32e1ae1784aaf09e4cefd5e51fcaeb1f4
github username <github email address>
2021-06-23 12:36:30 -0400

    made new route for creature patch to update new location of creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -6,4 +6,15 @@ class CreaturesController < ApplicationController
         render json: creatures
     end
 
+    def update
+        creature = Creature.find_by(id: params[:id])
+        creature.update(creature_params)
+        render json: creature, status: 200
+    end
+
+    private
+
+    def creature_params
+        params.permit(:creature_name, :tier_multiplier, :name, :species, :category, :tier, :top_speed, :mode_of_travel, :image, :available, :location)
+    end
 end
