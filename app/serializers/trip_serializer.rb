d425810b06e660ac05482ee6ce546e841b54bb5b
Isaac <izaroot327@gmail.com>
2021-06-24 18:22:31 -0400

    modified trip serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,4 +1,8 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :creature_id, :starting_location, :ending_location, :distance, :duration, :price, :creature
+  attributes :id, :created_at, :user_id, :creature_id, :starting_location, :ending_location, :distance, :duration, :price, :creature
   # belongs_to :creature
+
+  def created_at
+    self.object.created_at.to_s[0..10]
+  end
 end
